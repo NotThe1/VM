5adaed89dcf734d1a8fd27111c05663c57553753
NotThe1 <FrankMartyn1945@gmail.com>
2016-07-22 17:15:19 -0400

    wrote JUnit test for MemoryLoaderFromFile. Started to work on the in-machine disassembler

---
+++ VM/src/utilities/IOpCodeMap.java
@@ -0,0 +1,50 @@
+package utilities;
+
+/**
+ * 
+ * @author Frank Martyn
+ * 
+ *         Each opCode's structure is accessed thru this interface
+ */
+public interface IOpCodeMap {
+
+	/**
+	 * 
+	 * @param opCode
+	 *            - the key(opCode) for the operation
+	 * @return the number of bytes occupied by this instruction
+	 */
+	public int getSize(byte opCode);
+
+	/**
+	 * @param opCode
+	 *            - the key(opCode) for the operation
+	 * @return simple text explanation of the operation
+	 */
+	public String getFunction(byte opCode);
+
+	/**
+	 * 
+	 * @param opCode
+	 *            - the key(opCode) for the operation
+	 * @return the assembler code for this instruction - one with no arguments
+	 */
+	public String getAssemblerCode(byte opCode);
+
+	/**
+	 * 
+	 * @param opCode
+	 *            - the key(opCode) for the operation
+	 * @return the assembler code for this instruction - one with one argument
+	 */
+	public String getAssemblerCode(byte opCode, byte plusOne);
+
+	/**
+	 * 
+	 * @param opCode
+	 *            - the key(opCode) for the operation
+	 * @return the assembler code for this instruction - one with two arguments
+	 */
+	public String getAssemblerCode(byte opCode, byte plusOne, byte plusTwo);
+
+}// interface IOpCodeMap
