c742b544ef0e09116e4ceff8f2d664f777d87e7c
Frank Martyn <FrankMartyn1945@gmail.com>
2017-02-01 17:46:12 -0500

    Show code - add File & add File from List

--- VM/src/codeSupport/debug/ShowCode.java
+++ VM/src/codeSupport/debug/ShowCode.java
@@ -12,31 +12,50 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Set;
 import java.util.prefs.Preferences;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
-import javax.swing.JTextPane;
+import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
-import javax.swing.text.StyledDocument;
 
 import utilities.FilePicker;
 import utilities.menus.MenuUtility;
 
 public class ShowCode extends JDialog {
-	
+
 	private ShowFileAdapter showFileAdapter = new ShowFileAdapter();
-	private StyledDocument doc;
+	// private StyledDocument doc;
 	private Path newListPath;
+	private HashMap<String, Limits> fileList = new HashMap<String, Limits>();
+	private HashMap<String, String> listings = new HashMap<String, String>();
+
+	private int programCounter;
+	private int currentStart, currentEnd;
+	private String currentFilePath = null;
+	private boolean fileIsCurrent;
+
+	private boolean showLineNumber = true;
 
 	/**
 	 * Launch the application.
@@ -50,42 +69,203 @@ public class ShowCode extends JDialog {
 					dialog.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
-				}
-			}
+				}//try
+			}//run
 		});
-	}//main
-	
-	private void doAddFile(){
-		JFileChooser fc = newListPath == null ? FilePicker.getMemPicker() : FilePicker.getMemPicker(newListPath);
+	}// main
+
+	private void doAddFile() {
+		JFileChooser fc = (newListPath) == null ? FilePicker.getAsmPicker() : FilePicker.getAsmPicker(newListPath);
 		if (fc.showOpenDialog(null) == JFileChooser.CANCEL_OPTION) {
 			System.out.println("Bailed out of the open");
 			return;
 		} // if - open
+			// fc.getSelectedFile().getAbsolutePath()
 		newListPath = Paths.get(fc.getSelectedFile().getParent());
-		addFile( newListPath);
+		addFile(fc.getSelectedFile().getAbsolutePath());
+	}// doAddFile()
 
-	}//doAddFile()
-	
-	private void addFile(Path listFile){
-		
-	}//addFile
-	
-	private void doAddFilesFromList(){
-		
-	}//doAddFilesFromList
-	
-	private void doSaveSelectedToList(){
-		
-	}//doSaveSelectedTOList
-	
-	private void doClearSelectedFiles(){
+	private void addFile(String listFileFullPath) {
+		int endAddress = -1, startAddress = -1;
+		Pattern hotLineRegex = Pattern
+				.compile("(?<lineNumber>\\d{4}: )(?<address>[A-Fa-f\\d]{4})(?<junk> [A-Fa-f\\d]{2})");
+		Matcher matcherForHotLine;
+		StringBuilder stringBuilder = new StringBuilder();
+
+		try {
+			FileReader fileReader;
+			fileReader = new FileReader(listFileFullPath);
+			BufferedReader reader = new BufferedReader(fileReader);
+			String line;
+			int addressOnThisLine;
+			while ((line = reader.readLine()) != null) {
+				matcherForHotLine = hotLineRegex.matcher(line);
+				if (matcherForHotLine.find()) {
+					// String addressStr = matcherForHotLine.group("address");
+					addressOnThisLine = Integer.valueOf(matcherForHotLine.group("address"), 16);
+					endAddress = addressOnThisLine;
+					startAddress = startAddress == -1 ? addressOnThisLine : startAddress;
+				}//if - its a hot line
+				stringBuilder.append(line + System.lineSeparator());
+			} // while
+			reader.close();
+			tpDisplay.setText(stringBuilder.toString());
+		} catch (FileNotFoundException fnfe) {
+			JOptionPane.showMessageDialog(null, listFileFullPath + "not found", "unable to locate",
+					JOptionPane.ERROR_MESSAGE);
+			return; // exit gracefully
+		} catch (IOException ie) {
+			JOptionPane.showMessageDialog(null, listFileFullPath + ie.getMessage(), "IO error",
+					JOptionPane.ERROR_MESSAGE);
+			return; // exit gracefully
+		} // try
+		fileList.put(listFileFullPath, new Limits(startAddress, endAddress));
+		listings.put(listFileFullPath, stringBuilder.toString());
+		loadDisplay(listFileFullPath);
+		tpDisplay.setCaretPosition(0);
+		// reader.close();
+		MenuUtility.addItemToList(mnuFiles, new File(listFileFullPath), new JCheckBoxMenuItem());
+
+	}// addFile
+
+	private void loadDisplay(String filePath) {
+		lblHeader.setText(new File(filePath).getName());
+		lblHeader.setToolTipText(filePath);
+
+		tpDisplay.setText(listings.get(filePath));
+		Limits limits = fileList.get(filePath);
+		currentStart = limits.start;
+		currentEnd = limits.end;
+		currentFilePath = filePath;
+	}// loadDisplay
+
+	private void doAddFilesFromList() {
+		JFileChooser fc = FilePicker.getListAsmPicker();
+		if (fc.showOpenDialog(null) != JFileChooser.APPROVE_OPTION) {
+			System.out.printf("You cancelled the Load Asm from File List...%n", "");
+		} else {
+			FileReader fileReader;
+			String filePathName = null;
+			File currentFile;
+			try {
+				fileReader = new FileReader((fc.getSelectedFile().getAbsolutePath()));
+				BufferedReader reader = new BufferedReader(fileReader);
+				while ((filePathName = reader.readLine()) != null) {
+					addFile(filePathName);
+					// currentFile = new File(filePathName);
+					// loadMemoryFromFile(currentFile);
+				} // while
+				reader.close();
+			} catch (IOException e1) {
+				System.out.printf(e1.getMessage() + "%n", "");
+			} // try
+		} // if
+
+		return;
+	}// doAddFilesFromList
+
+	private void doSaveSelectedToList() {
+
+	}// doSaveSelectedTOList
+
+	private void doClearSelectedFiles() {
 		MenuUtility.clearListSelected(mnuFiles);
-	}//doClearSelectedFiles
-	
-	private void doClearAllFiles(){
+	}// doClearSelectedFiles
+
+	private void doClearAllFiles() {
 		MenuUtility.clearList(mnuFiles);
-	}//doClearFiles
-	
+	}// doClearFiles
+
+	// -------------------------------------------------------------------------------
+	private void clearCurrentIndicaters() {
+		currentFilePath = null;
+		currentStart = -1;
+		currentEnd = -1;
+		fileIsCurrent = false;
+	}// clearCurrentIndicaters
+
+	public void setProgramCounter(int programCounter) {
+		this.programCounter = programCounter & 0XFFFF;
+
+		setFileToShow(programCounter);
+
+		if (currentFilePath == null) {
+			// notified user already.
+			return; // not much do do
+		} // if
+
+		if (!fileIsCurrent) { // file is not current file
+			loadDisplay(currentFilePath);
+		} // if
+
+		selectTheCorrectLine();
+	}// setProgramCounter
+
+	private void setFileToShow(int lineNumber) {
+		// returns true if number is in the currently loaded file
+		fileIsCurrent = false;
+		if (isLineInCurrentFile(lineNumber)) {
+			fileIsCurrent = true;
+			return; // everything is in place
+		} // if its in current file
+
+		// Limits thisFilesLimit = new Limits();
+		boolean weHaveAFile = false;
+
+		Set<String> filePaths = fileList.keySet();
+		for (String filePath : filePaths) {
+			if (isLineInThisFile(lineNumber, filePath)) {
+				weHaveAFile = true;
+				currentFilePath = filePath;
+				Limits thisLimits = fileList.get(filePath);
+				currentStart = thisLimits.start;
+				currentEnd = thisLimits.end;
+				break;
+			} // if
+		} // for
+		lblStatus.setText("-");
+		if (!weHaveAFile) {
+			clearCurrentIndicaters();
+			String status = String.format("Target line: %04X Not In Any Currently Loaded Files%n", lineNumber);
+			lblStatus.setText(status);
+		} // if file not found
+		return;
+	}// getFileToShow
+
+	private boolean isLineInCurrentFile(int lineNumber) {
+		return ((lineNumber >= currentStart) && (lineNumber <= currentEnd));
+	}// isLineInCurrentFile
+
+	private boolean isLineInThisFile(int lineNumber, String filePath) {
+		boolean isLineInThisFile = false;
+		Limits thisFilesLimit = fileList.get(filePath);
+		if ((lineNumber >= thisFilesLimit.start) && (lineNumber <= thisFilesLimit.end)) {
+			currentFilePath = filePath;
+			currentStart = thisFilesLimit.start;
+			currentEnd = thisFilesLimit.end;
+			isLineInThisFile = true;
+		} // if
+		return isLineInThisFile;
+	}// isLineInThisFile
+
+	private void selectTheCorrectLine() {
+		String line;
+		String targetAddressRegex = String.format("\\d{4}: %04X [A-Fa-f\\d]{2}.*\r", programCounter);
+		Pattern targetAddressPattern = Pattern.compile(targetAddressRegex);
+		Matcher targetAddressMatcher;
+		targetAddressMatcher = targetAddressPattern.matcher(tpDisplay.getText());
+		if (targetAddressMatcher.find()) {
+			// System.out.printf("[selectTheCorrectLine] |%s|%n", targetAddressMatcher.group(0));
+			tpDisplay.setSelectionStart(targetAddressMatcher.start());
+			tpDisplay.setSelectionEnd(targetAddressMatcher.end());
+			lblStatus.setText("-");
+		} else {
+			String status = String.format("Target line: %04X Not Start of Instruction%n", programCounter);
+			lblStatus.setText(status);
+		} //
+
+	}// selectTheCorrectLine
+
 	// ===============================================================================
 	public void close() {
 		appClose();
@@ -109,12 +289,12 @@ public class ShowCode extends JDialog {
 		setLocation(myPrefs.getInt("LocX", 100), myPrefs.getInt("LocY", 100));
 		myPrefs = null;
 		this.setVisible(true);
-		
-		doc = tpDisplay.getStyledDocument();
+		// addFile1("C:\\Users\\admin\\git\\assembler8080\\assembler8080\\Code\\currentOS\\ROM.list");
+
+		clearCurrentIndicaters();
 	}// appInit
 
 	// ---------------------------------------------------------------------------------------
-
 
 	/**
 	 * Create the dialog.
@@ -129,122 +309,155 @@ public class ShowCode extends JDialog {
 		initialize();
 		appInit();
 	}//
-	
-	public void initialize(){
+
+	public void initialize() {
 		setTitle("Show Code");
-//			setBounds(100, 100, 450, 300);
-			GridBagLayout gridBagLayout = new GridBagLayout();
-			gridBagLayout.columnWidths = new int[]{0, 0};
-			gridBagLayout.rowHeights = new int[]{0, 30, 0};
-			gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-			gridBagLayout.rowWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
-			getContentPane().setLayout(gridBagLayout);
-			
-			JScrollPane scrollPane = new JScrollPane();
-			GridBagConstraints gbc_scrollPane = new GridBagConstraints();
-			gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
-			gbc_scrollPane.fill = GridBagConstraints.BOTH;
-			gbc_scrollPane.gridx = 0;
-			gbc_scrollPane.gridy = 0;
-			getContentPane().add(scrollPane, gbc_scrollPane);
-			
-			tpDisplay = new JTextPane();
-			tpDisplay.setFont(new Font("Courier New", Font.PLAIN, 14));
-			scrollPane.setViewportView(tpDisplay);
-			
-			JLabel label = new JLabel("No Active File");
-			label.setHorizontalAlignment(SwingConstants.CENTER);
-			label.setForeground(Color.BLUE);
-			label.setFont(new Font("Courier New", Font.BOLD, 16));
-			scrollPane.setColumnHeaderView(label);
-			
-			JPanel panel = new JPanel();
-			GridBagConstraints gbc_panel = new GridBagConstraints();
-			gbc_panel.fill = GridBagConstraints.BOTH;
-			gbc_panel.gridx = 0;
-			gbc_panel.gridy = 1;
-			getContentPane().add(panel, gbc_panel);
-			GridBagLayout gbl_panel = new GridBagLayout();
-			gbl_panel.columnWidths = new int[]{0};
-			gbl_panel.rowHeights = new int[]{0};
-			gbl_panel.columnWeights = new double[]{Double.MIN_VALUE};
-			gbl_panel.rowWeights = new double[]{Double.MIN_VALUE};
-			panel.setLayout(gbl_panel);
-			
-			JMenuBar menuBar = new JMenuBar();
-			setJMenuBar(menuBar);
-			
-			mnuFiles = new JMenu("Files");
-			menuBar.add(mnuFiles);
-			
-			JMenuItem mnuFilesAddFile = new JMenuItem("Add File");
-			mnuFilesAddFile.addActionListener(showFileAdapter);
-			mnuFiles.add(mnuFilesAddFile);
-			
-			JMenuItem mnuFilesAddFilesFromList = new JMenuItem("Add Files From List");
-			mnuFilesAddFilesFromList.addActionListener(showFileAdapter);
-			mnuFiles.add(mnuFilesAddFilesFromList);
-			
-			JSeparator separator = new JSeparator();
-			mnuFiles.add(separator);
-			
-			JMenuItem mnuSaveSelectedToList = new JMenuItem("Save Selected To List");
-			mnuSaveSelectedToList.addActionListener(showFileAdapter);
-			mnuFiles.add(mnuSaveSelectedToList);
-			
-			JSeparator separator_1 = new JSeparator();
-			separator_1.setName("recentFilesStart");
-			mnuFiles.add(separator_1);
-			
-			JSeparator separator_2 = new JSeparator();
-			separator_2.setName("recentFilesEnd");
-			mnuFiles.add(separator_2);
-			
-			JMenuItem mnuClearSelectedFiles = new JMenuItem("Clear Selected Files");
-			mnuClearSelectedFiles.addActionListener(showFileAdapter);
-			mnuFiles.add(mnuClearSelectedFiles);
-			
-			JMenuItem mnuFilesClearAllFiles = new JMenuItem("Clear All Files");
-			mnuFilesClearAllFiles.addActionListener(showFileAdapter);
-			mnuFiles.add(mnuFilesClearAllFiles);
-	
-	}//initialize
-	
-	class ShowFileAdapter implements ActionListener{
+		// setBounds(100, 100, 450, 300);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[] { 0, 0 };
+		gridBagLayout.rowHeights = new int[] { 0, 30, 0 };
+		gridBagLayout.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 1.0, 0.0, Double.MIN_VALUE };
+		getContentPane().setLayout(gridBagLayout);
+
+		JScrollPane scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 0;
+		gbc_scrollPane.gridy = 0;
+		getContentPane().add(scrollPane, gbc_scrollPane);
+
+		tpDisplay = new JTextArea();
+		tpDisplay.setEditable(false);
+		tpDisplay.setFont(new Font("Courier New", Font.PLAIN, 14));
+		scrollPane.setViewportView(tpDisplay);
+
+		lblHeader = new JLabel("No Active File");
+		lblHeader.setHorizontalAlignment(SwingConstants.CENTER);
+		lblHeader.setForeground(Color.BLUE);
+		lblHeader.setFont(new Font("Courier New", Font.BOLD, 16));
+		scrollPane.setColumnHeaderView(lblHeader);
+
+		JPanel panel = new JPanel();
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.fill = GridBagConstraints.BOTH;
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 1;
+		getContentPane().add(panel, gbc_panel);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[] { 0, 0 };
+		gbl_panel.rowHeights = new int[] { 0, 0 };
+		gbl_panel.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		gbl_panel.rowWeights = new double[] { 0.0, Double.MIN_VALUE };
+		panel.setLayout(gbl_panel);
+
+		lblStatus = new JLabel("-");
+		lblStatus.setForeground(Color.RED);
+		lblStatus.setFont(new Font("Arial", Font.BOLD, 17));
+		GridBagConstraints gbc_lblStatus = new GridBagConstraints();
+		gbc_lblStatus.anchor = GridBagConstraints.SOUTH;
+		gbc_lblStatus.gridx = 0;
+		gbc_lblStatus.gridy = 0;
+		panel.add(lblStatus, gbc_lblStatus);
+
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+
+		mnuFiles = new JMenu("Files");
+		menuBar.add(mnuFiles);
+
+		JMenuItem mnuFilesAddFile = new JMenuItem("Add File");
+		mnuFilesAddFile.setName(MNU_FILE_ADD_FILE);
+		mnuFilesAddFile.addActionListener(showFileAdapter);
+		mnuFiles.add(mnuFilesAddFile);
+
+		JMenuItem mnuFilesAddFilesFromList = new JMenuItem("Add Files From List");
+		mnuFilesAddFilesFromList.setName(MNU_FILE_ADD_FILES_FROM_LIST);
+		mnuFilesAddFilesFromList.addActionListener(showFileAdapter);
+		mnuFiles.add(mnuFilesAddFilesFromList);
+
+		JSeparator separator = new JSeparator();
+		mnuFiles.add(separator);
+
+		JMenuItem mnuSaveSelectedToList = new JMenuItem("Save Selected To List");
+		mnuSaveSelectedToList.setName(MNU_FILE_SAVE_SELECTED_TO_LIST);
+		mnuSaveSelectedToList.addActionListener(showFileAdapter);
+		mnuFiles.add(mnuSaveSelectedToList);
+
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setName("recentFilesStart");
+		mnuFiles.add(separator_1);
+
+		JSeparator separator_2 = new JSeparator();
+		separator_2.setName("recentFilesEnd");
+		mnuFiles.add(separator_2);
+
+		JMenuItem mnuClearSelectedFiles = new JMenuItem("Clear Selected Files");
+		mnuClearSelectedFiles.setName(MNU_CLEAR_SELECTED_FILES);
+		mnuClearSelectedFiles.addActionListener(showFileAdapter);
+		mnuFiles.add(mnuClearSelectedFiles);
+
+		JMenuItem mnuFilesClearAllFiles = new JMenuItem("Clear All Files");
+		mnuFilesClearAllFiles.setName(MNU_CLEAR_ALL_FILES);
+		mnuFilesClearAllFiles.addActionListener(showFileAdapter);
+		mnuFiles.add(mnuFilesClearAllFiles);
+
+	}// initialize
+
+	class ShowFileAdapter implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent actionEvent) {
-			String name = ((ActionEvent) actionEvent.getSource()).getActionCommand();
-			switch (name){
+			String name = ((JMenuItem) actionEvent.getSource()).getName();
+			switch (name) {
 			case MNU_FILE_ADD_FILE:
 				doAddFile();
 				break;
-				case MNU_FILE_ADD_FILES_FROM_LIST:
-					doAddFilesFromList();
+			case MNU_FILE_ADD_FILES_FROM_LIST:
+				doAddFilesFromList();
 				break;
-				case MNU_FILE_SAVE_SELECTED_TO_LIST:
-					doSaveSelectedToList();
+			case MNU_FILE_SAVE_SELECTED_TO_LIST:
+				doSaveSelectedToList();
 				break;
-				case MNU_CLEAR_SELECTED_FILES:
-					doClearSelectedFiles();
-					
+			case MNU_CLEAR_SELECTED_FILES:
+				doClearSelectedFiles();
 				break;
-				case MNU_CLEAR_ALL_FILES:
-					doClearAllFiles();
-					
+			case MNU_CLEAR_ALL_FILES:
+				doClearAllFiles();
 				break;
-			}
-			
-		}//actionPerformed
-		
-	}//class ShowFileAdapter
-	
+			}// switch
+
+		}// actionPerformed
+
+	}// class ShowFileAdapter
+
+	class Limits {
+		public int start;
+		public int end;
+
+		public Limits() {
+			this(-1, -1);
+		}// Constructor
+
+		public Limits(int start, int end) {
+			this.start = start;
+			this.end = end;
+		}// Constructor
+	}// class Limits
+
 	private static final String MNU_FILE_ADD_FILE = "mnuFileAddFile";
 	private static final String MNU_FILE_ADD_FILES_FROM_LIST = "mnuFileAddFilesFromList";
 	private static final String MNU_FILE_SAVE_SELECTED_TO_LIST = "mnuFilSaveSelectedToList";
 	private static final String MNU_CLEAR_SELECTED_FILES = "mnuClearSelectedFiles";
 	private static final String MNU_CLEAR_ALL_FILES = "mnuClearAllFiles";
-	private JMenu mnuFiles;
-	private JTextPane tpDisplay;
 
-}//class ShowCode
+	private static final String LEFT_P = "(";
+	private static final String RIGHT_P = ")";
+	private static final String MID_P = RIGHT_P + LEFT_P;
+	private JMenu mnuFiles;
+	private JTextArea tpDisplay;
+	private JLabel lblHeader;
+	private JLabel lblStatus;
+
+}// class ShowCode
---
+++ VM/test/codeSupport/DriveShowCode.java
@@ -0,0 +1,138 @@
+package codeSupport;
+
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SpinnerNumberModel;
+
+import codeSupport.debug.ShowCode;
+import utilities.hdNumberBox.HDNumberBox;
+
+public class DriveShowCode {
+	
+	private static ShowCode showCode;
+
+	private JFrame frame;
+	private JButton btnTxtLog;
+	private JScrollPane scrollPane;
+	private JTextArea txtLog;
+	private HDNumberBox hdNumber;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DriveShowCode window = new DriveShowCode();
+					window.frame.setVisible(true);
+					showCode = new ShowCode();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	
+
+	/**
+	 * Create the application.
+	 */
+	public DriveShowCode() {
+		
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	
+		frame = new JFrame();
+		frame.setBounds(100, 100, 429, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 20, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
+		
+		JButton btnStart = new JButton("Start");
+		btnStart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				showCode = new ShowCode();
+			}
+		});
+		GridBagConstraints gbc_btnStart = new GridBagConstraints();
+		gbc_btnStart.insets = new Insets(0, 0, 5, 5);
+		gbc_btnStart.gridx = 1;
+		gbc_btnStart.gridy = 0;
+		frame.getContentPane().add(btnStart, gbc_btnStart);
+		
+		JButton btnPC = new JButton("Send");
+		btnPC.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+//				showCode.setProgramCounter((int)spinner.getValue());
+				showCode.setProgramCounter((int)hdNumber.getValue());
+			}
+		});
+		
+		btnTxtLog = new JButton("Text Log");
+		btnTxtLog.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			txtLog.setVisible(!txtLog.isVisible());
+			}
+		});
+		GridBagConstraints gbc_btnTxtLog = new GridBagConstraints();
+		gbc_btnTxtLog.anchor = GridBagConstraints.ABOVE_BASELINE_TRAILING;
+		gbc_btnTxtLog.insets = new Insets(0, 0, 5, 5);
+		gbc_btnTxtLog.gridx = 1;
+		gbc_btnTxtLog.gridy = 1;
+		frame.getContentPane().add(btnTxtLog, gbc_btnTxtLog);
+		
+		scrollPane = new JScrollPane();
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 0);
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 4;
+		gbc_scrollPane.gridy = 2;
+		frame.getContentPane().add(scrollPane, gbc_scrollPane);
+		
+		txtLog = new JTextArea();
+		scrollPane.setViewportView(txtLog);
+		GridBagConstraints gbc_btnPC = new GridBagConstraints();
+		gbc_btnPC.insets = new Insets(0, 0, 0, 5);
+		gbc_btnPC.gridx = 1;
+		gbc_btnPC.gridy = 3;
+		frame.getContentPane().add(btnPC, gbc_btnPC);
+		
+//		hexSpinner = new Hex64KSpinner();
+//		GridBagConstraints gbc_hexSpinner = new GridBagConstraints();
+//		gbc_hexSpinner.insets = new Insets(0, 0, 0, 5);
+//		gbc_hexSpinner.gridx = 2;
+//		gbc_hexSpinner.gridy = 3;
+//		frame.getContentPane().add(hexSpinner, gbc_hexSpinner);
+		
+		hdNumber = new HDNumberBox();
+		hdNumber.setPreferredSize(new Dimension(50, 23));
+		hdNumber.setMinimumSize(new Dimension(20, 25));
+		hdNumber.setHexDisplay();
+		hdNumber.setNumberModel(new SpinnerNumberModel(0,0,0XFFFF,1));
+		GridBagConstraints gbc_hdNumber = new GridBagConstraints();
+		gbc_hdNumber.insets = new Insets(0, 0, 0, 5);
+		gbc_hdNumber.fill = GridBagConstraints.HORIZONTAL;
+		gbc_hdNumber.gridx = 2;
+		gbc_hdNumber.gridy = 3;
+		frame.getContentPane().add(hdNumber, gbc_hdNumber);
+	}
+
+}
